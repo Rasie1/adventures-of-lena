f3f9292e5944fbb8ea15d9667813f1ea0310ee43
Vsevolod Kvachev <rasielll@gmail.com>
2017-11-05 19:44:44 +0300

    Work on time

--- src/Main.hs
+++ src/Main.hs
@@ -12,21 +12,34 @@ import Control.Monad.Loops
 import Data.Foldable          
 import Data.Array
 
+import System.Clock
+import Control.Exception
+import Formatting
+import Formatting.Clock
+
 data Intent
   = Idle
   | Quit
 
-data World = World
+data GameState = GameState
   { exiting :: Bool
   , level :: Level
+  , currentTime :: TimeSpec
   }
 
-mkWorld :: Level -> World
-mkWorld lvl = World
+mkGameState :: Level -> TimeSpec -> GameState
+mkGameState lvl time = GameState
   { exiting = False
   , level = lvl
+  , currentTime = time
   }
 
+data Camera = Camera (Double, Double)
+
+type DeltaTime = Double
+
+diffTime :: TimeSpec -> TimeSpec -> DeltaTime
+diffTime end start = (* 1e-9) $ fromIntegral $ toNanoSecs end - toNanoSecs  start
 
 main :: IO ()
 main = C.withSDL $ C.withSDLImage $ do
@@ -36,25 +49,27 @@ main = C.withSDL $ C.withSDLImage $ do
       t <- C.loadTextureWithInfo r "./assets/tiles.png"
       levelString <- readFile "./assets/tiles.map"
 
-      let doRender = renderWorld r t
-      let initialWorld = mkWorld (loadLevel levelString)
-      let update x = updateWorld x <$> SDL.pollEvents
+      currentTime <- getTime Monotonic
+
+      let doRender = renderGameState r t
+      let initialGameState = mkGameState (loadLevel levelString) currentTime
+      let update x = updateGameState x <$> SDL.pollEvents
                      >>= \x' -> x' <$ doRender x'
 
-      runApp update initialWorld
+      runApp update initialGameState
 
       SDL.destroyTexture (fst t)
 
 
-runApp :: (Monad m) => (World -> m World) -> World -> m ()
+runApp :: (Monad m) => (GameState -> m GameState) -> GameState -> m ()
 runApp update = repeatUntil update exiting
 
 repeatUntil :: (Monad m) => (a -> m a) -> (a -> Bool) -> a -> m ()
 repeatUntil f p = go
   where go a = f a >>= \b -> unless (p b) (go b)
 
-updateWorld :: World -> [SDL.Event] -> World
-updateWorld w
+updateGameState :: GameState -> [SDL.Event] -> GameState
+updateGameState w
   = foldl' (flip applyIntent) w
   . fmap (payloadToIntent . SDL.eventPayload)
 
@@ -84,29 +99,29 @@ buttonIntent _ = Idle
 --            then Press
 --            else Release
 
-applyIntent :: Intent -> World -> World
-applyIntent Idle        = idleWorld
-applyIntent Quit        = quitWorld
+applyIntent :: Intent -> GameState -> GameState
+applyIntent Idle        = idleGameState
+applyIntent Quit        = quitGameState
 
 
-idleWorld :: World -> World
-idleWorld = id
+idleGameState :: GameState -> GameState
+idleGameState = id
 
 
-quitWorld :: World -> World
-quitWorld w = w { exiting = True }
+quitGameState :: GameState -> GameState
+quitGameState w = w { exiting = True }
 
 
-renderWorld :: SDL.Renderer -> (SDL.Texture, SDL.TextureInfo) -> World -> IO ()
-renderWorld r t w = do
+renderGameState :: SDL.Renderer -> (SDL.Texture, SDL.TextureInfo) -> GameState -> IO ()
+renderGameState r t w = do
   SDL.clear r
-  drawWorld r t w
+  drawGameState r t w (Camera (0, 0))
   SDL.present r
 
-drawWorld :: SDL.Renderer -> (SDL.Texture, SDL.TextureInfo) -> World -> IO ()
-drawWorld renderer (texture, ti) world = do
+drawGameState :: SDL.Renderer -> (SDL.Texture, SDL.TextureInfo) -> GameState -> Camera -> IO ()
+drawGameState renderer (texture, ti) world camera = do
     forM_ (assocs . tiles . level $ world) $ \((i, j), tile) ->
-      renderTile i j tile
+      renderTile i j tile camera
     where
       tileWidth :: Double
       tileWidth = (fromIntegral $ SDL.textureWidth ti) / 16
@@ -116,12 +131,13 @@ drawWorld renderer (texture, ti) world = do
       getTilesheetCoords Sky = (0, 0)
       getTilesheetCoords Grass = (64, 0)
 
-      renderTile x y t
+      renderTile x y t camera
         = SDL.copy renderer texture
             (Just $ floor <$> tileRect `moveTo` getTilesheetCoords t)
             (Just $ floor <$> tileRect `moveTo` (fromIntegral x * tileWidth, fromIntegral y * tileWidth))
 
-
+applyCamera :: Camera -> SDL.Rectangle a -> SDL.Rectangle a
+applyCamera c r = r
 
 moveTo :: SDL.Rectangle a -> (a, a) -> SDL.Rectangle a
 moveTo (SDL.Rectangle _ d) (x, y) = SDL.Rectangle (C.mkPoint x y) d
